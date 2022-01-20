08a243f2e8c3bf13944af4232aacec673ec64b21
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-20 12:54:46 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -6,7 +6,7 @@ const PersonContext = createContext();
 const App = ()=> {
     const [person, setPerson] = useState(data);
     return(<div className="App component">
-        <PersonContext.Provider value={[person, setPerson]}>
+        <PersonContext.Provider value={[person, setPerson, "Warren"]}>
             <h1>Main App</h1>
             <SubComp1 />
         </PersonContext.Provider>
@@ -44,7 +44,7 @@ const SubComp2 = () => {
 }
 
 const SubComp3 = () => {
-    const [person, setPerson] = useContext(PersonContext);
+    const [person, setPerson, example] = useContext(PersonContext);
 
     const handleClick = ()=> {
         setPerson({
@@ -59,6 +59,7 @@ const SubComp3 = () => {
 
     return(<div className="component">
         <h1>Sub Comp 3</h1>
+        {example}
         <h2>{person.location.street} {person.location.city}, {person.location.state} {person.location.postcode}</h2>
         <button onClick={handleClick}>Change Name</button>
     </div>)
