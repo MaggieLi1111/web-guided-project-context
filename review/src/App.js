c0d2a6835b32547f255fb62f51ee175d7d4e5206
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-20 13:01:44 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -66,6 +66,7 @@ const SubComp3 = () => {
 
     return(<div className="component">
         <h1>Sub Comp 3</h1>
+        <p>{highSchoolName}</p>
         <h2>{person.location.street} {person.location.city}, {person.location.state} {person.location.postcode}</h2>
         <button onClick={handleClick}>Change Name</button>
     </div>)
